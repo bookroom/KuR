d0b4ade2b964ab08bafaeb954d24149d33158a76
houhonglei <houhonglei@bbktel.com>
2017-03-17 17:55:04 +0800

    初始化提交

---
+++ app/src/main/java/wu/hei/xiao/ku/KuActivity.java
@@ -0,0 +1,13 @@
+package wu.hei.xiao.ku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ku);
+    }
+}
